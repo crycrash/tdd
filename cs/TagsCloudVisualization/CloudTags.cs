627f5379121f6696386b8b6ca5d5fde545abdceb
crycrash <monstermusic.club@yandex.ru>
2024-11-13 17:09:18 +0500

    A spiral has been built into the addition of rectangles and addition tests have been added

--- cs/TagsCloudVisualization/CloudTags.cs
+++ cs/TagsCloudVisualization/CloudTags.cs
@@ -4,16 +4,21 @@ namespace Layouter;
 
 class CircularCloudLayouter
 {
-    Spiral spiral;
-    Point centercloud;
+    private Spiral spiral;
+    private Point centercloud;
 
-    public Point Center => centercloud;
+    private List<Rectangle> rectangles;
+
+    public Point GetCenter => centercloud;
+
+    public List<Rectangle> GetRectangles => rectangles;
     
     public CircularCloudLayouter(Point center){
         this.centercloud = center;
         this.spiral = new Spiral(center);
+        this.rectangles = new List<Rectangle>();
     }
-    
+
     public Rectangle PutNextRectangle(Size rectangleSize){
         if (rectangleSize.IsEmpty){
             throw new ArgumentNullException("rectangle is empty");
@@ -21,6 +26,21 @@ class CircularCloudLayouter
         if (rectangleSize.Height <= 0 || rectangleSize.Width <= 0){
             throw new ArgumentOutOfRangeException("side less or equal zero");
         }
-        return Rectangle.Empty;
+        Rectangle tempRectangle;
+        do{
+            Point nextPoint = spiral.GetNextPoint();
+            tempRectangle = new Rectangle(nextPoint, rectangleSize);
+        }
+        while(IsRectangleIntersect(tempRectangle));
+        rectangles.Add(tempRectangle);
+        return tempRectangle;
+    }
+
+    private bool IsRectangleIntersect(Rectangle rectangleChecked){
+        foreach(Rectangle rectangle in rectangles){
+            if (rectangleChecked.IntersectsWith(rectangle))
+                return true;
+        }
+        return false;
     }
 }
--- cs/TagsCloudVisualization/TestsTagsCloudVisualization.cs
+++ cs/TagsCloudVisualization/TestsTagsCloudVisualization.cs
@@ -6,8 +6,7 @@ namespace Layouter;
 
 public class TestsCloudVisualization
 {
-
-    CircularCloudLayouter circularCloudLayouter;
+    private CircularCloudLayouter circularCloudLayouter;
     [SetUp]
     public void SetUp(){
         circularCloudLayouter = new CircularCloudLayouter(new Point(0, 0));
@@ -17,8 +16,8 @@ public class TestsCloudVisualization
     public void CircularCloudLayouter_SettingCenter()
     {
         CircularCloudLayouter circularLayouter = new CircularCloudLayouter(new Point(2, 4));
-        circularLayouter.Center.X.Should().Be(2);
-        circularLayouter.Center.Y.Should().Be(4);
+        circularLayouter.GetCenter.X.Should().Be(2);
+        circularLayouter.GetCenter.Y.Should().Be(4);
     }
 
     [Test]
@@ -39,4 +38,93 @@ public class TestsCloudVisualization
         action.Should().Throw<ArgumentNullException>().Which.Message.Should().Contain("rectangle is empty");
     }
 
+    [Test]
+    public void CircularCloudLayouter_RectanglesEmptyAfterInitialization(){
+        circularCloudLayouter.GetRectangles.Should().BeEmpty();
+    }
+
+    [Test]
+    public void PutNextRectangle_PutFirstRectangle(){
+        Size rectangleSize = new Size(3, 7);
+        Rectangle expectedRectangle = new Rectangle(new Point(0, 0), rectangleSize);
+        circularCloudLayouter.PutNextRectangle(rectangleSize);
+
+        circularCloudLayouter.GetRectangles.Count().Should().Be(1);
+        circularCloudLayouter.GetRectangles.Should().Contain(expectedRectangle);
+    }
+
+    [Test]
+    public void PutNextRectangle_PutSeveralRectangles(){
+        Size rectangleSize = new Size(3, 7);
+        for(int i = 0;i < 20;i++){
+            circularCloudLayouter.PutNextRectangle(rectangleSize);
+        }
+        circularCloudLayouter.GetRectangles.Count().Should().Be(20);
+        circularCloudLayouter.GetRectangles.Should().AllBeOfType(typeof(Rectangle));
+    }
+
+    [Test]
+    public void PutNextRectangle_LessThanAreaOfCircle()
+    {
+        var rectanglesSizes = new List<Size>
+        {
+            new Size(10, 5),
+            new Size(8, 8),
+            new Size(12, 3),
+            new Size(6, 10)
+        };
+
+        foreach (var size in rectanglesSizes)
+        {
+            circularCloudLayouter.PutNextRectangle(size);
+        }
+
+        int totalRectangleArea = circularCloudLayouter.GetRectangles
+            .Sum(rect => rect.Width * rect.Height);
+
+        int maxRadiusFromCenter = (int)circularCloudLayouter.GetRectangles
+            .Max(rect => GetMaxDistanceToCorner(rect, circularCloudLayouter.GetCenter));
+
+        int circleArea = (int)(Math.PI * Math.Pow(maxRadiusFromCenter, 2));
+        totalRectangleArea.Should().BeLessThan(circleArea);
+    }
+
+    private double GetMaxDistanceToCorner(Rectangle rect, Point center)
+    {
+        var corners = new[]
+        {
+            new Point(rect.Left, rect.Top),
+            new Point(rect.Right, rect.Top),
+            new Point(rect.Left, rect.Bottom),
+            new Point(rect.Right, rect.Bottom)
+        };
+        return corners.Max(corner => Distance(center, corner));
+    }
+
+    private double Distance(Point p1, Point p2)
+    {
+        return (int)Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+    }
+
+    [Test]
+    public void PutNextRectangle_SeveralRectanglesDontIntersect(){
+        var rectanglesSizes = new List<Size>
+        {
+            new Size(10, 5),
+            new Size(8, 8),
+            new Size(12, 3),
+            new Size(6, 10)
+        };
+
+        foreach (var size in rectanglesSizes)
+        {
+            circularCloudLayouter.PutNextRectangle(size);
+        }
+
+        List<Rectangle> rectanglesTemp = circularCloudLayouter.GetRectangles;
+        for (int i = 0; i < rectanglesTemp.Count; i++){
+            for (int j = i + 1; j < rectanglesTemp.Count; j++)
+                rectanglesTemp[i].IntersectsWith(rectanglesTemp[j]).Should().BeFalse();
+        }  
+    }
 }
